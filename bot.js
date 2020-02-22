4b85f569756393735ad0309a339c3f1dfa4b3322
Loh Sze Ying <bloom9334@hotmail.com>
2020-02-23 00:26:45 +0800

    Initial Commit

---
+++ bot.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = "NjgwNzk3MDg3NTA3ODA4Mjcw.XlFIUg.UWmMc_0m3XXfHx3nHCrtTQBmKHs";
+const PREFIX = '!';
+
+bot.on('ready', () => {
+    console.log('Online!');
+})
+
+bot.on('message', msg => {
+    let args = msg.content.substring(PREFIX.length).split(" ");
+    switch (args[0]) {
+        case 'ping':
+            //msg.channel.sendMessage('pong!');
+            msg.reply('pong!')
+            break;
+        case 'randomnum':
+            var ran = Math.floor(Math.random() * 10000000);
+            msg.channel.sendMessage(ran);
+            break;
+        case 'codetest':
+            msg.channel.sendCode("Java", "Hello");
+        case 'embed':
+            const embed = new Discord.RichEmbed()
+            .setTitle("User info")
+            .addField('Name', msg.author.username)
+            //.setThumbnail(msg.author.avatarURL)
+            .attachFiles(['DaUser.png'])
+            .setImage('attachment://DaUser.png')
+            .setFooter('TEST FOOTER')
+            .setColor(0xF1C40F)
+            msg.channel.sendEmbed(embed);
+        default:
+            break;
+    }
+}) 
+
+bot.login(token);
